183f354f3ff440d5cd85cec06c9f9032f06d3929
Raymond <raymond13gggg@gmail.com>
2020-09-01 19:51:24 -0700

    more updates

--- lab2/Main.java
+++ lab2/Main.java
@@ -20,30 +20,30 @@ public class Main{
         int bugYPosition = 0;
     }
 
-    public static char menu(){
+    public static char menu(int xPosition, int yPosition){
         System.out.println("Which Direction:\nW. Up\nA. Left\nS. Down\nD. Right");
         Scanner usrInput = new Scanner(System.in);
         char charInput = usrInput.next().charAt(0);
         charInput = Character.toLowerCase(charInput); // to make sure the character is lower case for while loop comparisons
 
         while(charInput != 'w' || charInput != 'a' || charInput != 's' || charInput != 'd'){// go into loop at least once to check if charInput is actually valid
-            if(charInput == 'w'){
+            if(charInput == 'w' && xPosition > 0){
                 usrInput.close();
                 return charInput;
             }
-            else if(charInput == 'a'){
+            else if(charInput == 'a' && yPosition > 0){
                 usrInput.close();
                 return charInput;
             }
-            else if(charInput == 's'){
+            else if(charInput == 's' && xPosition < 9){
                 usrInput.close();
                 return charInput;
             }
-            else if (charInput == 'd'){
+            else if (charInput == 'd' && yPosition < 9){
                 usrInput.close();
                 return charInput;
             }
-            System.out.println("\nSorry, please enter one of the characters above");
+            System.out.println("\nSorry, please enter a valid character or move");
             System.out.println("Which Direction:\nW. Up\nA. Left\nS. Down\nD. Right");
             charInput = usrInput.next().charAt(0);
         }
@@ -51,5 +51,5 @@ public class Main{
         return charInput;
     }
 
-  
+    
 }
