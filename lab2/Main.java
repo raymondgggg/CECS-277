c3e93f75c555dfb3adb6ec5c8f45dbc12461fd49
Raymond <raymond13gggg@gmail.com>
2020-09-01 20:48:12 -0700

    somewhat working game

--- lab2/Main.java
+++ lab2/Main.java
@@ -18,25 +18,27 @@ public class Main{
         int bugXPosition = 0;
         int bugYPosition = 0;
         
-        
-        displayGrid(grid, bugYPosition, bugXPosition);
-        char updatedPosition = menu(bugXPosition, bugYPosition);
-        if(updatedPosition == 'w'){
-            bugYPosition--;
-        }
-        else if(updatedPosition == 'a'){
-            bugXPosition++;
-        }
-        else if (updatedPosition == 's'){
-            bugYPosition++;
-        }
-        else if (updatedPosition == 'd'){
-            bugXPosition++;
-        }
-        System.out.println(bugXPosition + " " + bugYPosition);
+        while (bugXPosition !=9 && bugYPosition != 9){
+            displayGrid(grid, bugXPosition, bugYPosition);
+            char updatedPosition = menu(bugXPosition, bugYPosition);
+            if(updatedPosition == 'w'){
+                grid[bugYPosition][bugXPosition] = 'x';
+                bugYPosition--;
+            }
+            else if(updatedPosition == 'a'){
+                grid[bugYPosition][bugXPosition] = 'x';
+                bugXPosition++;
+            }
+            else if (updatedPosition == 's'){
+                grid[bugYPosition][bugXPosition] = 'x';
+                bugYPosition++;
+            }
+            else if (updatedPosition == 'd'){
+                grid[bugYPosition][bugXPosition] = 'x';
+                bugXPosition++;
+            }
 
-
-
+        }
     }
 
     public static char menu(int xPosition, int yPosition){
@@ -47,39 +49,35 @@ public class Main{
 
         while(charInput != 'w' || charInput != 'a' || charInput != 's' || charInput != 'd'){// go into loop at least once to check if charInput is actually valid
             if(charInput == 'w' && xPosition > 0){
-                usrInput.close();
+                System.out.println();
                 return charInput;
             }
             else if(charInput == 'a' && yPosition > 0){
-                usrInput.close();
+                System.out.println();
                 return charInput;
             }
             else if(charInput == 's' && xPosition < 9){
-                usrInput.close();
+                System.out.println();
                 return charInput;
             }
             else if (charInput == 'd' && yPosition < 9){
-                usrInput.close();
+                System.out.println();
                 return charInput;
             }
             System.out.println("\nSorry, please enter a valid character or move");
             System.out.println("Which Direction:\nW. Up\nA. Left\nS. Down\nD. Right");
             charInput = usrInput.next().charAt(0);
         }
-        usrInput.close();
         return charInput;
     }
 
     public static void displayGrid(char grid[][], int xPosition, int yPosition){
-        if (xPosition == 0 && yPosition == 0){
-            grid[xPosition][yPosition] = 'o';
-            for (int i = 0; i < grid.length; i++){
-                for (int j = 0; j < grid.length; j++){
-                    System.out.print(grid[i][j] + " ");
-                }
-                System.out.println();
+        grid[yPosition][xPosition] = 'o';
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid.length; j++){
+                System.out.print(grid[i][j] + " ");
             }
-        
+            System.out.println();
         }
     }
     
