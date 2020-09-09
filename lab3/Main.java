fad7c97df938c35f3c22aed582db812f896d9946
Raymond <raymond13gggg@gmail.com>
2020-09-08 18:57:57 -0700

    readFile

--- lab3/Main.java
+++ lab3/Main.java
@@ -1,14 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-
-public class Main{
+public class Main {
     public static void main(String[] args) {
+        ArrayList<String> states = new ArrayList<String>();
+        ArrayList<Integer> population = new ArrayList<Integer>();
         menu();
+        readFile(states, population);
     }
 
-
-    public static void menu(){
+    public static void menu() {
         System.out.println("State Stats\n1.Display Sorted States\n2.Display Sorted Populations");
         System.out.println("3.Display Total US population\n4.Display State With Population Greater than\n6.Quit");
+    }
+
+    public static void readFile(ArrayList<String> states, ArrayList<Integer> population) {
+        File inputFile = new File("StatePops.txt");
+        try {
+            Scanner input = new Scanner(inputFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
