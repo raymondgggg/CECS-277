36bd492228ae1e4186e6981ed1f77caddade2638
Raymond <raymond13gggg@gmail.com>
2020-09-08 18:43:55 -0700

    lab 3

---
+++ lab3/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[] args) {
+        menu();
+    }
+
+
+    public static void menu(){
+        System.out.println("State Stats\n1.Display Sorted States\n2.Display Sorted Populations");
+        System.out.println("3.Display Total US population\n4.Display State With Population Greater than\n6.Quit");
+    }
+}
