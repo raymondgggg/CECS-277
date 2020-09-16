eaa4252ff29f4ff0e017ffb7b5bbd7e21c8e3d9f
Raymond <raymond13gggg@gmail.com>
2020-09-15 21:08:17 -0700

    docs

--- lab4/Main.java
+++ lab4/Main.java
@@ -1,7 +1,18 @@
+// Raymond Guevara Lozano 
+// Juan Anaya
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    
+    /** 
+     * Main method where the 30x30 char array is initialized, as well as the array list of 
+     * type Rect. User is prompted to enter desired number of rectangles and other methods
+     * within the main class are called to append Rect objects to arraylist and modify existing
+     * grid with new rectangles.
+     * @param args No command line arguments used in this program
+     */
     public static void main(String[] args) {
         char[][] charArray = new char[30][30]; //initialization of 30x30 grid of chars 
         ArrayList<Rect> rectArrayList = new ArrayList<Rect>(); //intialization of rect array
@@ -24,6 +35,12 @@ public class Main {
 
     }
 
+    
+    /** 
+     * Method for displaying the 30x30 grid of chars where 
+     * the rectangles will eventually be put 
+     * @param grid 2D Array that contains the char characters
+     */
     public static void displayGrid(char grid[][]){
         for (int i = 0; i < grid.length; i++){
             for (int j = 0; j < grid.length; j++){
@@ -33,12 +50,21 @@ public class Main {
         }
     }
 
+    
+    /** 
+     * Method for prompting user to give values that are needed 
+     * to instantiate the Rect object. Uses input validation to 
+     * make sure that the fields are within the rang for the 2D
+     * array.
+     * @param currentRect Number of current rectangle that is being instantiated
+     * @return r Rectangle object made after getting fields for object from user
+     */
     public static Rect getRect(int currentRect){
         System.out.print("Please enter x location of rectangle " + currentRect + " (1-30): ");
         int x = getIntRange(1, 30);
         System.out.print("\nPlease enter y location of of rectangle " + currentRect + " (1-30): ");
         int y = getIntRange(1, 30);
-        System.out.print("\nPlease enter the width of rectangel " + currentRect + "(1-25): ");
+        System.out.print("\nPlease enter the width of rectangle " + currentRect + "(1-25): ");
         int width = getIntRange(1, 25);
         System.out.print("\nPlease enter the height of rectangle " + currentRect + " (1-25): ");
         int height = getIntRange(1, 25);
@@ -48,6 +74,14 @@ public class Main {
 
     }
 
+    
+    /** 
+     * Method for filling rectangle grid with the rectangle objects and corresponding 
+     * char character  
+     * @param grid 2D Array containing the char values
+     * @param rectangle Object that will be used to fill grid
+     * @param c Character that will be used to represent the rectangle object
+     */
     public static void fillRect(char grid[][], Rect rectangle, char c){
         for (int i = 0; i<rectangle.getWidth(); i++){
             for(int j = 0; j<rectangle.getHeight(); j++){
@@ -56,6 +90,15 @@ public class Main {
         }
     }
 
+    
+    /**
+     * Checks if the inputted value is an integer and within the specified range
+     * (ex: 1-10)
+     * 
+     * @param low  lower bound of the range.
+     * @param high upper bound of the range.
+     * @return the valid input.
+     */
     public static int getIntRange(int low, int high) {
         Scanner in = new Scanner(System.in);
         int input = 0;
@@ -75,9 +118,6 @@ public class Main {
         }
         return input;
     }
-
-
-
 }
 
 
--- lab4/Rect.java
+++ lab4/Rect.java
@@ -4,6 +4,13 @@ public class Rect{
     private int width;
     private int height;
 
+    /**
+     * Constructor of rectangle object
+     * @param x value of rectangle 
+     * @param y value of rectangle
+     * @param width of rectangle 
+     * @param height of rectangle 
+     */
     public Rect(int x, int y, int width, int height){
         this.x = x;
         this.y = y; 
@@ -11,18 +18,38 @@ public class Rect{
         this.height = height;
     }
 
+    
+    /** 
+     * Method for returning the x-value of rectangle
+     * @return int X-value
+     */
     public int getX(){
         return this.x;
     }
 
+    
+    /** 
+     * Method for returning the y-value of rectangle 
+     * @return int Y-value
+     */
     public int getY(){
         return this.y;
     }
 
+    
+    /** 
+     * Method for returning the width of rectangle 
+     * @return int Width
+     */
     public int getWidth(){
         return this.width;
     }
 
+    
+    /** 
+     * Method for returning the height of rectangle
+     * @return int Height
+     */
     public int getHeight(){
         return this.height;
     }
