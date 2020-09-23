a04e05720b1b681512c80b282de1eeed50866c83
Raymond <raymond13gggg@gmail.com>
2020-09-22 22:56:45 -0700

    fixed bugs

--- lab5/Main.java
+++ lab5/Main.java
@@ -1,6 +1,7 @@
 //Raymond Guevara Lozano
 //Catherine Bui
 //Quoc Ngo
+
 /**Main class where the program is executed */
 public class Main {
     /** Method where the user is prompted to roll dice and
@@ -10,11 +11,12 @@ public class Main {
     public static void main(String[] args) {
        boolean usrInput;
        Player player = new Player();
+       System.out.println("Yahtzee\n");
        do {
-           System.out.println("Yahtzee\n");
            player.takeTurn();
            System.out.println("Play again? (Y/N) ");
            usrInput = CheckInput.getYesNo(); // input validation 
+           System.out.println();
        }while(usrInput == true);
        System.out.println("Game Over.");
        System.out.println("Final Score = " + player.getPoints() + " point(s)"); // final score
--- lab5/Player.java
+++ lab5/Player.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 /** Player Class - representation of player for dice game */
 public class Player {
     /** Array of dice the player will use in game */
@@ -8,20 +7,19 @@ public class Player {
 
     /** Constructor - creates dice array and initializes points at
      *  start of game to zero
-     * 
      */
     public Player(){
-        dice = new Die[3];
+        this.dice = new Die[3];
         Die d;
         for (int i =0; i < dice.length; i++){
             d = new Die();
-            dice[i] = d;
+            this.dice[i] = d;
         }
         this.points = 0;
     }
 
     /** String representation of the dice array
-     * @return String of the dice values for the game
+     * @return String representation of the dice values for the game
      */
     public String toString(){
         String values = "";
@@ -31,10 +29,10 @@ public class Player {
         return values;
     }
 
-    /**
-     * Method to sort the die objects in dice array in non-decreasing order
+    /**Method to sort the die objects in dice array in non-decreasing order
+     * based of the die values
      */
-    public void sort(){
+    public Die[] sort(){
         for (int i = 0; i < this.dice.length; i++){
             int minIndex = i;
             for (int j = i + 1; j < this.dice.length; j++){
@@ -46,10 +44,11 @@ public class Player {
             this.dice[minIndex] = this.dice[i];
             this.dice[i] = temp;
         }
+        return this.dice;
     }
 
     /** Return the number of points the Player currently has in the game
-     * @return int number of points the player currently has
+     * @return points the player currently has
      */
     public int getPoints(){
         return this.points;
@@ -70,7 +69,7 @@ public class Player {
     }
 
     /** Method for comparing if two of the dice in the array are the same
-     *  updates player score by 1
+     *  updates player points by 1 if true
      * @return boolean returns true of two values are the same, false otherwise
      */
     public boolean twoOfKind(){
@@ -85,13 +84,14 @@ public class Player {
     }
 
     /** Method for checking if the dice values are in a consecutive sequence with 
-     *  one another 
+     *  one another updates player points by 2 if true
      * @return boolean value true if values are in sequence, false otherwise
      */
     public boolean series(){
-        int diceValues[] = {this.dice[0].getDieValue(), this.dice[1].getDieValue(), this.dice[2].getDieValue()};
-        Arrays.sort(diceValues);
-        if ((diceValues[1] - diceValues[0] == 1) && (diceValues[2] - diceValues[0] == 2)){
+        sort();
+        int die2To1Difference = this.dice[1].getDieValue() - this.dice[0].getDieValue();
+        int die3To2Difference = this.dice[2].getDieValue() - this.dice[0].getDieValue();
+        if ((die2To1Difference == 1) && (die3To2Difference == 2)){
             this.points += 2;
             return true;
         }
@@ -106,13 +106,14 @@ public class Player {
         for (int i = 0; i < this.dice.length; i++) {
             this.dice[i].roll();
         }
-        System.out.println("Rolling Dice..." + toString());
-        if (twoOfKind()){
-            System.out.println("You got a pair!");
+        sort();
+        System.out.println("Rolling Dice..." + this.toString());
+        if (threeOfKind()) {
+            System.out.println("You got three of a kind!");
             System.out.println("Score = " + this.points + " point(s)");
         }
-        else if(threeOfKind()){
-            System.out.println("You got three of a kind!");
+        else if (twoOfKind()){
+            System.out.println("You got a pair!");
             System.out.println("Score = " + this.points + " point(s)");
         }
         else if(series()){
@@ -120,7 +121,6 @@ public class Player {
             System.out.println("Score = " + this.points + " point(s)");
         }
         else{
-
             System.out.println("Awww. Too Bad.");
             System.out.println("Score = " + this.points + " point(s)");
         }
