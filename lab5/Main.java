ce3e30acf1d85d6ce5369b07d8cfe14c5037026c
Raymond <raymond13gggg@gmail.com>
2020-09-22 22:01:27 -0700

    update

--- lab5/Main.java
+++ lab5/Main.java
@@ -8,14 +8,13 @@ public class Main {
      * @param args no command line arguments used for the main method
      */
     public static void main(String[] args) {
-       CheckInput checkInput = new CheckInput();
        boolean usrInput;
        Player player = new Player();
        do {
            System.out.println("Yahtzee\n");
            player.takeTurn();
            System.out.println("Play again? (Y/N) ");
-           usrInput = checkInput.getYesNo(); // input validation 
+           usrInput = CheckInput.getYesNo(); // input validation 
        }while(usrInput == true);
        System.out.println("Game Over.");
        System.out.println("Final Score = " + player.getPoints() + " point(s)"); // final score
