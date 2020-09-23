685d348a0ccc6ff1e6374d45f526c00077eef4c3
Raymond <raymond13gggg@gmail.com>
2020-09-22 19:13:40 -0700

    player

---
+++ lab5/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player();
+        System.out.println(player.toString());
+
+    }
+}
---
+++ lab5/Player.java
@@ -0,0 +1,35 @@
+public class Player {
+    private Die[] dice;
+    private int points;
+
+    public Player(){
+        dice = new Die[3];
+        Die d;
+        for (int i =0; i < dice.length; i++){
+            d = new Die();
+            dice[i] = d;
+        }
+    }
+
+    public String toString(){
+        String values = "";
+        for (int d = 0; d < this.dice.length; d++){
+            values += "D" + (d+1) + ": " + this.dice[d] + " ";
+        }
+        return values;
+    }
+
+    public void sort(){
+        for (int i = 0; i < this.dice.length; i++){
+            int minIndex = i;
+            for (int j = i + 1; j < this.dice.length; j++){
+                if(this.dice[j].lessThan(this.dice[minIndex])){
+                    minIndex = j;
+                }
+            }
+            Die temp = this.dice[minIndex];
+            
+        }
+
+    }
+}
