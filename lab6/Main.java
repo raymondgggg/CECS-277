e226b987a02ffb4dec702e5fcaaa1553d0a69049
Raymond <raymond13gggg@gmail.com>
2020-09-29 22:07:35 -0700

    refactoring

--- lab6/Main.java
+++ lab6/Main.java
@@ -1,20 +1,31 @@
+// Catherine Bui
+// Quoc Ngo
+// Raymond Guevara Lozano
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-
+/**Main class where the Cipher class and CaesarCipher class are all glued together
+ * to make functioning program with input validation for Lab 6
+ */
 public class Main {
+    /** Main method where the user is prompted with the choice of
+     *  encrypting or decrypting and based off choices is allowed to input 
+     *  that will be encrypted to file using either Atbash or Caesar Cipher, Or will
+     *  decrypt using either Atbash or Caesar Cipher the text that is present in 
+     *  "message.txt"
+     * @param args No command line arguments take for this program
+     */
     public static void main(String[] args) {
-        // Cipher c = new Cipher();
-        // System.out.println(c.encrypt("1. Raymond Guevara"));
-        // System.out.println(c.decrypt("1. IZBNLMW TFVEZIZ"));
-
-        // CaesarCipher cc = new CaesarCipher(1);
-        // System.out.println(cc.encrypt("1. aabbcc"));
-        // System.out.println(cc.decrypt("1. bbccdd"));
         System.out.println("Secret Decoder Ring:\n1.Encrypt\n2.Decrypt");
         int encryptOrDecrypt = CheckInput.getIntRange(1, 2);
-        System.out.println("Enter Decryption Type:\n1.Atbash\n2.Caesar");
+        if (encryptOrDecrypt == 2){
+            System.out.println("Enter Decryption Type:\n1.Atbash\n2.Caesar");
+        }
+        else{
+            System.out.println("Enter Encryption Type:\n1.Atbash\n2.Caesar");
+        }
+        
         int encryptOrDecryptType = CheckInput.getIntRange(1, 2);
 
         if (encryptOrDecrypt == 1 && encryptOrDecryptType == 1) {
@@ -28,26 +39,32 @@ public class Main {
             System.out.print("Enter message: ");
             String message = CheckInput.getString();
             System.out.print("Enter shift value: ");
-            int shift = CheckInput.getInt();
+            int shift = CheckInput.getIntRange(1,25);
             CaesarCipher cc = new CaesarCipher(shift);
             String encryptedMsg = cc.encrypt(message);
             printToFile(encryptedMsg);
         }
         else if (encryptOrDecrypt == 2 && encryptOrDecryptType == 1){
             String message = readFile();
-            System.out.println("Reading encrypted message from \"message.txt\"");
+            System.out.println("Reading encrypted message from \"message.txt\".");
             Cipher c = new Cipher();
             System.out.println("Decrypted Message: " + c.decrypt(message));
         }
         else if (encryptOrDecrypt == 2 && encryptOrDecryptType == 2){
             String message = readFile();
+            System.out.println("Reading encrypted message from \"message.txt\"");
             System.out.print("Enter shift value: ");
-            int shift = CheckInput.getInt();
+            int shift = CheckInput.getIntRange(1,25);
             CaesarCipher cc = new CaesarCipher(shift);
             System.out.println("Decrypted Message: " + cc.decrypt(message));
         }
 
     }
+    
+    /** Method for printing the encrypted string to the file
+     *  message.txt
+     * @param encryptedMsg string that has been encrypted
+     */
     public static void printToFile(String encryptedMsg){
         try {
             PrintWriter out = new PrintWriter("message.txt");
@@ -60,15 +77,23 @@ public class Main {
         }
     }
 
+    
+    /** Method for reading from the file "message.txt" and returning
+     *  the encrypted text from the file so that it can be used in 
+     *  the main method.
+     * @return String that is in the file
+     */
     public static String readFile(){
         File inputFile = new File("message.txt");
         try{
             Scanner input = new Scanner(inputFile);
             String message = input.nextLine();
+            input.close();
             return message;
         } catch(FileNotFoundException e){
             e.printStackTrace();
+            return null;// return value if there is no file
         }
-        return null;// return value if there is no file
+     
     }
 }
