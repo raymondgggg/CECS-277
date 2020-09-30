e9ea20fc10cc4d5631eb5a9235e18d70325de4b3
Raymond <raymond13gggg@gmail.com>
2020-09-29 19:08:57 -0700

    test Cipher.java

--- lab6/Main.java
+++ lab6/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main{
     public static void main(String[] args) {
         Cipher c = new Cipher();
-        System.out.println(c.decryptLetter('z'));
+        System.out.println(c.encrypt("apple"));
+        System.out.println(c.decrypt("zkkov"));
     }
 }
