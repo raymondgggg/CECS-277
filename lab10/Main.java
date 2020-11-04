e14ea08b5e5397de3e54cd7fa7cc0ee44669fd54
Raymond <raymond13gggg@gmail.com>
2020-11-03 19:55:25 -0800

    storePattern method

--- lab10/Main.java
+++ lab10/Main.java
@@ -1,24 +1,21 @@
 import java.util.HashMap;
-
+/** Main class where all the methods are stored to make program run */
 public class Main {
-    
     /** 
-     * @param args
+     * @param args takes no command line arguments
      */
     public static void main(String[] args) {
     
     }
 
-    
     /** 
      * @param prevEntries
      * @param patternString
      */
     public static void makePrediction(HashMap<String,Integer> prevEntries, String patternString){
-
+        
     }
 
-    
     /** 
      * Method to prompt the user to enter the input for the prediction game, has input validation 
      * to make sure the input is either X, O, or Q
@@ -41,7 +38,17 @@ public class Main {
         return input;
     }
 
-    public static void storePattern(){
-
+    /**
+     * Method to store the patter based off the given pattern string, if patternstring already in 
+     * map, then the count of associated with the key is updated, otherwise, new entry is created and count is
+     * set to 1.
+     */
+    public static void storePattern(HashMap<String, Integer> prevEntries, String patternString){
+        if (prevEntries.containsKey(patternString)){
+            int prevCount = prevEntries.get(patternString);
+            prevEntries.put(patternString, prevCount++);
+            return;
+        }
+        prevEntries.put(patternString, 1);
     }
 }
